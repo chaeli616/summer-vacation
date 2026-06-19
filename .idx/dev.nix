0eb473633018729ee8184a72f786598d9d00e3dc
imchaeli1234 <imchaeli1234@gmail.com>
2026-06-19 23:07:05 +0000

    Initialize project and add changes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -14,7 +14,7 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
-      "google.gemini-cli-vscode-ide-companion"
+      # "google.gemini-cli-vscode-ide-companion"
     ];
     # Enable previews and customize configuration
     previews = {
